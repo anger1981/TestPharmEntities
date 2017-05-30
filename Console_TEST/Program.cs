4d5947d9fe7c0cc60c1cb64345de7e7bad79878c
anger1981 <anger1981@rambler.ru>
2017-05-31 01:35:02 +0500

    1

--- Console_TEST/Program.cs
+++ Console_TEST/Program.cs
@@ -10,6 +10,12 @@ namespace Console_TEST
     {
         static void Main(string[] args)
         {
+            PharmEntities db = new PharmEntities();
+            var prod_10 = db.Products.Take(10);
+            foreach (Product p in prod_10)
+                Console.WriteLine("ID {0}, name {1}, group {2}", p.Id_Product, p.Name_full, p.Id_product_group);
+
+            Console.ReadLine();
         }
     }
 }
